f6e76f0b843bcf47d834349122b7766114328f10
robertsj <rschultzejena@gmail.com>
2024-09-03 11:42:54 +0200

    Simple form submission for new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { render partial: 'users/thank_you' }
+      else
+        format.html { render partial: 'users/error', locals: { errors: @user.errors.full_messages } }
+      end
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :public_key)
   end
 end
