e2ae3534cc8b4d366f2137c97c8690b16fbae6de
robertsj <rschultzejena@gmail.com>
2024-09-04 12:06:24 +0200

    Write system spec for submitting usernames
    I switched from the default chromedriver used by selenium-webdriver to
    rack-test, which sped things up considerably and avoided errors from
    chromedriver:
    
    https://rspec.info/features/6-0/rspec-rails/system-specs/system-specs/

--- Gemfile
+++ Gemfile
@@ -69,7 +69,7 @@ end
 group :test do
   # Use system testing [https://guides.rubyonrails.org/testing.html#system-testing]
   gem "capybara"
-  gem "selenium-webdriver"
+  gem "selenium-webdriver", "4.24.0"
 end
 
 gem "tailwindcss-rails", "~> 2.7"
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root "users#new"
   resources :users, only: [:create]
 
-  get '/.well-known/nostr.json', to: 'verification#serve_json'
+  get '/.well-known/nostr.json', to: 'verification#serve_json', as: :nostr_json
 end
