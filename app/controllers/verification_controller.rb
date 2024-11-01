9b3e7d0d026623c1abba2d4015c82969a406aa61
robertsj <rschultzejena@gmail.com>
2024-11-01 15:46:53 +0100

    Set CORS header from Controller
    The CORS header wasn't properly set in the HTTP response, despiute
    adding and configuring the middleware rack-cors.
    
    This commit removes the hardcoding of the HTTP header in the test
    environment, which was a temporary fix to make the cors_header test
    pass.

--- app/controllers/verification_controller.rb
+++ app/controllers/verification_controller.rb
@@ -1,6 +1,13 @@
 class VerificationController < ApplicationController
+  before_action :set_cors_header
 
   def serve_json
     render :json => JsonCreatorService.new.content(name: params[:name])
+  end
+
+  private
+
+  def set_cors_header
+    headers['Access-Control-Allow-Origin'] = '*'
   end
 end
